d162f44bc2fec8677799608dbdcdc81b3ca1965e
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-11-06 12:34:12 +0100

    Init

---
+++ sobel-filter/sobel-filter/sobel-filter.cpp
@@ -0,0 +1,10 @@
+﻿extern "C" int _fastcall MyProc1(long long x, long long y);
+
+int main(int argc, char* argv[])
+{
+	int x = 3, y = 4, z = 0;
+
+	z = MyProc1(x, y);		// wywołanie procedury asemblerowej z biblioteki
+
+	return 0;
+}
