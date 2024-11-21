b061668f094fec24279bdfb60a2176ff4f63c734
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-11-21 13:03:53 +0100

    New base project

---
+++ sobel-filter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace sobel_filter
+{
+    internal class Program
+    {
+        [DllImport(@"C:\Users\oliwi\Documents\GitHub\sobel-filter\sobel-filter\x64\Debug\sobel-dll.dll")]
+        static extern int MyProc1(int a, int b);
+        static void Main(string[] args)
+        {
+            int x = 5, y = 3;
+            int retVal = MyProc1(x, y);
+            Console.WriteLine(retVal);
+            Console.ReadLine();
+        }
+    }
+}
